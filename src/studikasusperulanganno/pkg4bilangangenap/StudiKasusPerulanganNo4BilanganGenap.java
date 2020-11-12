5cf0f752ab4095afc12394e030e4cfb7c662ed29
LathifaPuteri <lathifa_puteri_29rpl@student.smktelkom-mlg.sch.id>
2020-11-12 23:49:04 +0700

    Perulangan Genap

---
+++ src/studikasusperulanganno/pkg4bilangangenap/StudiKasusPerulanganNo4BilanganGenap.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studikasusperulanganno.pkg4bilangangenap;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class StudiKasusPerulanganNo4BilanganGenap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println("Bilangan Genap dari 0 sampai "+n+" adalah:");
+        for (int i=0;i<=n;i++){
+        if (i % 2 == 0){
+            System.out.println(i + "");
+        }
+        
+    }
+    }
+    
+}
